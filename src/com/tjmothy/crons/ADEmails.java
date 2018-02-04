f91cc71d877838af9b5ff65049b80b404e2a235b
tmckeown <tmckeown@unboundmedicine.com>
2018-02-04 13:43:49 -0500

    cron for emailing ADs of coaches that missed reporting games

--- src/com/tjmothy/crons/ADEmails.java
+++ src/com/tjmothy/crons/ADEmails.java
@@ -3,7 +3,6 @@ package com.tjmothy.crons;
 import com.tjmothy.email.Email;
 import com.tjmothy.email.Emailer;
 import com.tjmothy.email.ReminderBean;
-import com.tjmothy.stats.Game;
 
 import javax.mail.MessagingException;
 import java.util.ArrayList;
@@ -14,13 +13,14 @@ import java.util.ArrayList;
 public class ADEmails
 {
     final static String REMINDER_SUBJECT = "Distict Scores: Reminder";
-    final static String REMINDER_BODY = "Hope you are having a great day. Friendly reminder that some of your coaches haven't submitted their scores for today's games.";
+    final static String REMINDER_BODY = "Hope you are having a great day. Friendly reminder that one or more of your coaches haven't submitted their scores for today's game.";
 
     public static void main(String[] args)
     {
         if (args.length == 0)
         {
             System.out.println("Empty args, no sport specified");
+            return;
         }
         else
         {
@@ -28,18 +28,27 @@ public class ADEmails
             try
             {
                 sportId = Integer.parseInt(args[0]);
-            } catch (NumberFormatException nfe)
+            }
+            catch (NumberFormatException nfe)
             {
-
+                System.out.println("ADEmail.main() - " + nfe.getMessage());
             }
 //		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy_MM_dd");
             ReminderBean rb = new ReminderBean();
-            String adEmail = rb.getADEmail(sportId);
-            System.out.println("sending Athletic Directory reminder email to -> " + adEmail);
-            sendEmail(REMINDER_BODY, REMINDER_SUBJECT, adEmail);
+            ArrayList<String> adEmails = rb.getADEmail(sportId);
+            System.out.println("Gathering AD emails for sportId: " + sportId);
+            adEmails.forEach((email) -> System.out.print("  Emailing Athletic Directory [" + email + "]"));
+//            sendEmail(REMINDER_BODY, REMINDER_SUBJECT, adEmail);
         }
     }
 
+    /**
+     * Send emails to Athletic Directors that some of their coahces haven't submitted scores;
+     *
+     * @param body
+     * @param subjectLine
+     * @param userEmail
+     */
     private static void sendEmail(String body, String subjectLine, String userEmail)
     {
         Email email = new Email(false);
@@ -49,7 +58,8 @@ public class ADEmails
         try
         {
             new Emailer(email);
-        } catch (MessagingException me)
+        }
+        catch (MessagingException me)
         {
             me.printStackTrace();
         }
--- src/com/tjmothy/email/ReminderBean.java
+++ src/com/tjmothy/email/ReminderBean.java
@@ -1,9 +1,6 @@
 package com.tjmothy.email;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 
 import com.tjmothy.stats.StatsBean;
@@ -12,167 +9,168 @@ import com.tjmothy.utils.TProperties;
 
 public class ReminderBean
 {
-	
-	private TProperties tProps;
 
-	public ReminderBean()
-	{
-		this.tProps = new TProperties();
-	}
+    private TProperties tProps;
 
-	/**
-	 * Retrieves a list of all teamIds that haven't submitted their games for the current day.
-	 * Then collects the email associated with that teamId.
-	 * 
-	 * @param sportId
-	 * @return
-	 */
-	public ArrayList<String> getEmailsForReminder(int sportId)
-	{
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		ArrayList<String> reminderEmails = new ArrayList<>();
+    public ReminderBean()
+    {
+        this.tProps = new TProperties();
+    }
 
-		try
-		{
-			Class.forName(TProperties.DRIVERS);
-			conn = DriverManager.getConnection(tProps.getConnection());
+    /**
+     * Retrieves a list of all teamIds that haven't submitted their games for the current day.
+     * Then collects the email associated with that teamId.
+     *
+     * @param sportId
+     * @return
+     */
+    public ArrayList<String> getEmailsForReminder(int sportId)
+    {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        ArrayList<String> reminderEmails = new ArrayList<>();
+
+        try
+        {
+            Class.forName(TProperties.DRIVERS);
+            conn = DriverManager.getConnection(tProps.getConnection());
 //			pstmt = conn.prepareStatement("SELECT s.id, s.home_id FROM schedule s WHERE s.home_id NOT IN(SELECT ts.team_id FROM team_stats ts WHERE ts.schedule_id = s.id AND ts.submitted = 0) AND s.game_day = CURDATE() AND s.sport=?");
-			pstmt = conn.prepareStatement("SELECT s.id, s.home_id FROM schedule s WHERE s.id NOT IN (SELECT ts.schedule_id FROM team_stats ts) AND s.game_day = CURDATE() AND s.sport=?");
-			pstmt.setInt(1, sportId);
-			rs = pstmt.executeQuery();
-			while (rs.next())
-			{
-				int teamId = rs.getInt(StatsBean.Column.home_id.name());
-				User user = getUserFromTeamId(teamId);
-				if (user != null)
-				{
-					reminderEmails.add(getUserFromTeamId(teamId).getEmail());
-				}
-				else
-				{
-					System.out.println("ReminderBean.getEmailsForReminder() -> user null for teamId = " + teamId);
-				}
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println("ReminderBean.getEmailsForReminder(): " + e.getMessage());
-		}
-		finally
-		{
-			try
-			{
-				if (conn != null)
-					conn.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (rs != null)
-					rs.close();
-			}
-			catch (Exception e)
-			{
-				;
-			}
-		}
-		return reminderEmails;
-	}
+            pstmt = conn.prepareStatement("SELECT s.id, s.home_id FROM schedule s WHERE s.id NOT IN (SELECT ts.schedule_id FROM team_stats ts) AND s.game_day = CURDATE() AND s.sport=?");
+            pstmt.setInt(1, sportId);
+            rs = pstmt.executeQuery();
+            while (rs.next())
+            {
+                int teamId = rs.getInt(StatsBean.Column.home_id.name());
+                User user = getUserFromTeamId(teamId);
+                if (user != null)
+                {
+                    reminderEmails.add(getUserFromTeamId(teamId).getEmail());
+                }
+                else
+                {
+                    System.out.println("ReminderBean.getEmailsForReminder() -> user null for teamId = " + teamId);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("ReminderBean.getEmailsForReminder(): " + e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (conn != null)
+                    conn.close();
+                if (pstmt != null)
+                    pstmt.close();
+                if (rs != null)
+                    rs.close();
+            }
+            catch (Exception e)
+            {
+                ;
+            }
+        }
+        return reminderEmails;
+    }
 
-	/**
-	 * Creates a new user object from the teamId
-	 * 
-	 * @param teamId
-	 * @return
-	 */
-	private User getUserFromTeamId(int teamId)
-	{
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		User user = null;
-		try
-		{
-			Class.forName(TProperties.DRIVERS);
-			conn = DriverManager.getConnection(tProps.getConnection());
-			pstmt = conn.prepareStatement("SELECT * FROM users WHERE team_id=? LIMIT 1");
-			pstmt.setInt(1, teamId);
-			rs = pstmt.executeQuery();
-			while (rs.next())
-			{
-				user = new User(rs);
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println("ReminderBean.getUserFromTeamId(): " + e.getMessage());
-		}
-		finally
-		{
-			try
-			{
-				if (conn != null)
-					conn.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (rs != null)
-					rs.close();
-			}
-			catch (Exception e)
-			{
-				;
-			}
-		}
-		return user;
-	}
+    /**
+     * Creates a new user object from the teamId
+     *
+     * @param teamId
+     * @return
+     */
+    private User getUserFromTeamId(int teamId)
+    {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        User user = null;
+        try
+        {
+            Class.forName(TProperties.DRIVERS);
+            conn = DriverManager.getConnection(tProps.getConnection());
+            pstmt = conn.prepareStatement("SELECT * FROM users WHERE team_id=? LIMIT 1");
+            pstmt.setInt(1, teamId);
+            rs = pstmt.executeQuery();
+            while (rs.next())
+            {
+                user = new User(rs);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("ReminderBean.getUserFromTeamId(): " + e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (conn != null)
+                    conn.close();
+                if (pstmt != null)
+                    pstmt.close();
+                if (rs != null)
+                    rs.close();
+            }
+            catch (Exception e)
+            {
+                ;
+            }
+        }
+        return user;
+    }
 
-	/**
-	 * Gets the Athletic Director's email for the specific sport
-	 *
-	 * @param sportId
-	 * @return - Athletic Director's email
-	 */
-	public String getADEmail(int sportId)
-	{
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String adEmail = "";
-		try
-		{
-			Class.forName(TProperties.DRIVERS);
-			conn = DriverManager.getConnection(tProps.getConnection());
-			pstmt = conn.prepareStatement("SELECT distinct user_email FROM wordpress.wp_users join teams " +
-					"on teams.wp_ADID = wp_users.id where teams.wp_ADID IN " +
-					"(SELECT wp_ADID FROM teams join schedule on (home_id = teams.id or road_id = teams.id) " +
-					"where game_day < now() and (home_score = 0 and road_score = 0) and (schedule.sport = ?);");
-			pstmt.setInt(1, sportId);
-			rs = pstmt.executeQuery();
-			while (rs.next())
-			{
-				adEmail = rs.getString("user_email");
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println("ReminderBean.getADEmail(): " + e.getMessage());
-		}
-		finally
-		{
-			try
-			{
-				if (conn != null)
-					conn.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (rs != null)
-					rs.close();
-			}
-			catch (Exception e)
-			{
-				;
-			}
-		}
-		return adEmail;
-	}
+    /**
+     * Gets the Athletic Director's email for the specific sport
+     *
+     * @param sportId
+     * @return - Athletic Director's email
+     */
+    public ArrayList<String> getADEmail(int sportId)
+    {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        ArrayList<String> emails = new ArrayList<>();
+        try
+        {
+            Class.forName(TProperties.DRIVERS);
+            conn = DriverManager.getConnection(tProps.getConnection());
+            pstmt = conn.prepareStatement("SELECT DISTINCT user_email FROM wordpress.wp_users " +
+                    "JOIN teams ON teams.wp_ADID = wp_users.id WHERE teams.wp_ADID IN " +
+                    "(SELECT wp_ADID FROM teams JOIN schedule ON (home_id = teams.id or road_id = teams.id) " +
+                    "WHERE game_day < NOW() AND (home_score = 0 AND road_score = 0) AND schedule.sport = ?);");
+            pstmt.setInt(1, sportId);
+            rs = pstmt.executeQuery();
+            while (rs.next())
+            {
+                System.out.println("email: " + rs.getString("user_email"));
+                emails.add(rs.getString("user_email"));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("ReminderBean.getADEmail(): " + e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (conn != null)
+                    conn.close();
+                if (pstmt != null)
+                    pstmt.close();
+                if (rs != null)
+                    rs.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("ReminderBean.getADEmail() - SQL connection error: " + e.getMessage());
+            }
+        }
+        return emails;
+    }
 }
